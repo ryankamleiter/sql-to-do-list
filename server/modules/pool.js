a4f9fbb682ac6fb0d1dd96385f10b0b8d10b411d
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-02-05 16:15:59 -0600

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,24 @@
 const pg = require('pg')
-
+let pool;
 let databaseName = 'weekend-to-do-app'
 
 if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
-
-const pool = new pg.Pool({
+if(process.env.DATABASE_URL){
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+  });
+}
+else {
+  pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: databaseName,
-    allowExitOnIdle: true 
-})
-
+    allowExitOnIdle: true
+  })
+};
 module.exports = pool
