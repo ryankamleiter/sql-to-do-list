124a62c391eb9169b6c34c4a2fb490a581917fb7
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-02-06 08:59:22 -0600

    changed database name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,15 +1,15 @@
 const pg = require('pg')
 let pool;
-let databaseName = 'weekend-to-do-app'
+let databaseName = 'weekend-to-do-list'
 
 if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
-if(process.env.DATABASE_URL){
+if (process.env.DATABASE_URL) {
   pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false
+      rejectUnauthorized: false
     }
   });
 }
